9ce7b6af52f0878da6d580dbac22dbce336a7dab
Robbie Prokop <robbieprokop@gmail.com>
2022-08-18 21:59:24 -0500

    install express-async-handler for errorHandling

--- backend/controllers/receiptController.js
+++ backend/controllers/receiptController.js
@@ -1,42 +1,43 @@
+const asyncHandler = require("express-async-handler");
 // @desc:   Get All Receipts
 //@route:   GET /api/receipts
 //@access   Private
-const getAllReceipts = (req, res) => {
+const getAllReceipts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all receipts" });
-};
+});
 
 // @desc:   Get One Receipt
 //@route:   GET /api/receipts/:id
 //@access   Private
-const getOneReceipt = (req, res) => {
+const getOneReceipt = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get receipt #${req.params.id}` });
-};
+});
 
 // @desc:   Create Receipt
 //@route:   POST /api/receipts
 //@access   Private
-const createReceipt = (req, res) => {
+const createReceipt = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please complete all text fields");
   }
 
   res.status(200).json({ message: `Create new receipt` });
-};
+});
 
 // @desc:   Update Receipt
 //@route:   PUT /api/receipts/:id
 //@access   Private
-const updateReceipt = (req, res) => {
+const updateReceipt = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update receipt #${req.params.id}` });
-};
+});
 
 // @desc:   Delete Receipt
 //@route:   DELETE /api/receipts/:id
 //@access   Private
-const deleteReceipt = (req, res) => {
+const deleteReceipt = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete receipt #${req.params.id}` });
-};
+});
 
 module.exports = {
   getAllReceipts,
