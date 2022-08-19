e6bff0ce843b53cdb9db52b637854b5c1a4217ec
Robbie Prokop <robbieprokop@gmail.com>
2022-08-19 10:58:39 -0500

    create user model

--- backend/models/receiptModel.js
+++ backend/models/receiptModel.js
@@ -2,9 +2,41 @@ const mongoose = require("mongoose");
 
 const receiptSchema = mongoose.Schema(
   {
-    text: {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    place: {
       type: String,
-      required: [true, "Please add a text value"],
+      required: [true, "Please add a location"],
+    },
+    firstName: {
+      type: String,
+      required: [true, "Please add a first name"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please add a last name"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please add an address"],
+    },
+    amount: {
+      number: {
+        type: Number,
+        required: [true, "Please add a donation amount"],
+      },
+      words: {
+        type: String,
+        required: [true, "Please add a donation amount"],
+      },
+    },
+
+    signature: {
+      type: String,
+      required: [true, "Please add a signature"],
     },
   },
   {
