e1f333e4ab60a97bc74e07d441f594e6d297fa13
Robbie Prokop <robbieprokop@gmail.com>
2022-08-22 17:29:08 -0500

    create receipt slice and service with getAll

--- frontend/src/features/receipts/receiptService.js
+++ frontend/src/features/receipts/receiptService.js
@@ -1,1 +1,16 @@
 import axios from "axios";
+
+const API_URL = "/api/users/me";
+
+//get all receipts
+const getAll = async () => {
+  const { data } = await axios.get(API_URL);
+
+  return data;
+};
+
+const receiptService = {
+  getAll,
+};
+
+export default receiptService;
