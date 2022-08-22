2f2da2c01170491237587ccc3d3ed1b3ecf66b8d
Robbie Prokop <robbieprokop@gmail.com>
2022-08-22 17:11:31 -0500

    start dashboard and redirect to login if no user

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
+
   return <div className="dashboard">Dashboard</div>;
 };
 export default Dashboard;
