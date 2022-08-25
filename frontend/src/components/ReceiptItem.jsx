8336d4c9620bc39ff0390c96513e25e83a84fe68
Robbie Prokop <robbieprokop@gmail.com>
2022-08-25 11:24:49 -0500

    styling receiptItem as grid

--- frontend/src/components/ReceiptItem.jsx
+++ frontend/src/components/ReceiptItem.jsx
@@ -1,27 +1,36 @@
 import { useDispatch } from "react-redux";
 import { deleteReceipt } from "../features/receipts/receiptSlice";
+import { Link } from "react-router-dom";
 
 const ReceiptItem = ({ receipt }) => {
   const dispatch = useDispatch();
+
   return (
-    <div className="receipt-form">
-      <div>{new Date(receipt.createdAt).toLocaleString("en-GB")}</div>
-      <h2>{receipt.receiptNumber}</h2>
+    <div className="receipt-item">
       <div>
-        <h4>ID: {receipt.receiptNumber}</h4>
         <div>
-          {/* <Link to={`/${receipt._id}`} className="btn btn-edit">
-            Edit
-          </Link> */}
-          <button
-            //This would work if I had access to the backend. Need the deletePost to return the post.id in order to filter it out.
-            onClick={() => dispatch(deleteReceipt(receipt._id))}
-            className="btn btn-delete"
-          >
-            Delete
-          </button>
+          {new Date(receipt.createdAt).toLocaleString("en-GB", {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
+          })}
+        </div>
+        <div>
+          <div>
+            <Link to={`/${receipt._id}`} className="btn btn-edit">
+              Edit
+            </Link>
+            <button
+              //This would work if I had access to the backend. Need the deletePost to return the post.id in order to filter it out.
+              onClick={() => dispatch(deleteReceipt(receipt._id))}
+              className="btn btn-delete"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
+      <h2>Receipt ID: {receipt.receiptNumber}</h2>
 
       <h3>
         Donor: {receipt.firstName} {receipt.lastName}
