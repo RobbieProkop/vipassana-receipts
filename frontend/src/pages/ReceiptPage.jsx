8e4f505ad060b442aeb6e746c48da872b4bd9783
Robbie Prokop <robbieprokop@gmail.com>
2022-09-05 15:26:16 -0500

    begin adding info to receipt page

--- frontend/src/pages/ReceiptPage.jsx
+++ frontend/src/pages/ReceiptPage.jsx
@@ -11,40 +11,66 @@ const ReceiptPage = () => {
   const receipt = useSelector((state) => {
     return state.receipts.receiptsArr.filter((receipt) => receipt._id === id);
   });
-  console.log("receipt", receipt);
+  console.log("receipt", receipt[0]);
 
   useEffect(() => {}, [dispatch, receipt]);
 
   return (
-    <div className="receipt-item">
+    <div className="receipt-page">
+      <div className="receipt-head">
+        <div className="receipt-info">
+          <h1>Alberta Vipassana Foundation</h1>
+          <p>PO Box 8412 - Market Mall</p>
+          <p>Calgary, AB, T3A 5C4</p>
+          <p>Charitable Reg. #85502 1739 RR 0001</p>
+          <p>
+            Canadian Revenue Agency: <a>www.cra-arc.gc.ca</a>
+          </p>
+        </div>
+        <h3>
+          N<sub>0</sub>: {receipt[0].receiptNumber}
+        </h3>
+      </div>
       <div>
-        <div>
-          {new Date(receipt.createdAt).toLocaleString("en-GB", {
+        <h3>
+          Date:{" "}
+          {new Date(receipt[0].createdAt).toLocaleString("en-GB", {
             year: "numeric",
             month: "numeric",
             day: "numeric",
           })}
-        </div>
-        <div>
-          <div>
-            <Link to={`/edit/${receipt._id}`} className="btn btn-edit">
-              Edit
-            </Link>
-            <button
-              onClick={() => dispatch(deleteReceipt(receipt._id))}
-              className="btn btn-delete"
-            >
-              Delete
-            </button>
-          </div>
-        </div>
+        </h3>
+        <h3>Place: {receipt[0].place}</h3>
       </div>
-      <h2>ID: {receipt.receiptNumber}</h2>
       <div>
         <h3>
-          Donor: {receipt.firstName} {receipt.lastName}
+          Donor: {receipt[0].firstName} {receipt[0].lastName}
         </h3>
-        <h3>Amount: ${receipt.number}</h3>
+        <h3>
+          Address: {receipt[0].address}, {receipt[0].postalCode}
+        </h3>
+      </div>
+      <div>
+        <h3>
+          Donation: ${receipt[0].number} {receipt[0].type}{" "}
+        </h3>
+        <h3>Total Amount Received: {receipt[0].words} </h3>
+        <h3>DIgital Signature: {receipt[0].signature}</h3>
+      </div>
+
+      {/* edit and delete */}
+      <div>
+        <div>
+          <Link to={`/edit/${receipt[0]._id}`} className="btn btn-edit">
+            Edit
+          </Link>
+          <button
+            onClick={() => dispatch(deleteReceipt(receipt[0]._id))}
+            className="btn btn-delete"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
