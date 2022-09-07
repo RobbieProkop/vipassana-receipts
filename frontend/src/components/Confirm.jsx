ac3776997139feeffad7f5239dfb48a4ad5d0b05
Robbie Prokop <robbieprokop@gmail.com>
2022-09-07 13:50:34 +0200

    create confirm component

--- frontend/src/components/Confirm.jsx
+++ frontend/src/components/Confirm.jsx
@@ -1,4 +1,18 @@
 const Confirm = () => {
-  return <div>Confirm</div>;
+  const onCancel = () => {};
+  const onDelete = () => {};
+  return (
+    <main className="card confirm">
+      <h1>Are you sure you want to delete?</h1>
+      <section className="actions">
+        <button className="cancel" onClick={onCancel}>
+          Cancel
+        </button>
+        <button className="onDelete" onClick={onDelete}>
+          Delete
+        </button>
+      </section>
+    </main>
+  );
 };
 export default Confirm;
