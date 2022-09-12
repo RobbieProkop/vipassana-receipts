935d70fe4967c7f0dcbd96784151fe56a6147920
Robbie Prokop <robbieprokop@gmail.com>
2022-09-12 15:03:48 +0400

    move btns out of downloadable pdf

--- frontend/src/pages/ReceiptPage.jsx
+++ frontend/src/pages/ReceiptPage.jsx
@@ -20,67 +20,84 @@ const ReceiptPage = () => {
   });
 
   const generatePDF = () => {
+    // html2canvas($("#canvas"), {
+    //   onrendered: function (canvas) {
+    //     var imgData = canvas.toDataURL("image/png");
+    //     var doc = new jsPDF("l", "px");
+    //     doc.addImage(imgData, "PNG", 10, 10);
+    //     doc.save(`AVF-tax-receipt-${receiptDate}.pdf`);
+    //   },
+    // });
+    // Bring the page zoom level back to 100%
+
     var doc = new jsPDF("l", "px");
 
-    doc.text(20, 20, "This is the first title.");
-    doc.text(20, 60, "This is the second title.");
-    doc.text(20, 100, "This is the thrid title.");
+    // doc.text(20, 20, "This is the first title.");
+    // doc.text(20, 60, "This is the second title.");
+    // doc.text(20, 100, "This is the thrid title.");
 
-    doc.save(`AVF-tax-receipt-${receiptDate}.pdf`);
+    doc.html(document.getElementById("receipt"), {
+      callback: function (doc) {
+        doc.save(`AVF-tax-receipt-${receiptDate}.pdf`);
+      },
+    });
+
+    // doc.save(`AVF-tax-receipt-${receiptDate}.pdf`);
   };
 
   useEffect(() => {}, [dispatch, receipt]);
 
   return (
-    <div className="receipt-page">
-      <div className="receipt-info">
-        <div className="receipt-head">
-          <h3>
-            N<sub>0</sub>. {receipt[0].receiptNumber}
-          </h3>
-          <h1>Alberta Vipassana Foundation</h1>
+    <main className="receipt-page">
+      <div id="receipt">
+        <div className="receipt-info">
+          <div className="receipt-head">
+            <h3>
+              N<sub>0</sub>. {receipt[0].receiptNumber}
+            </h3>
+            <h1>Alberta Vipassana Foundation</h1>
 
-          {/* delete receipt */}
-          <button
-            className="btn btn-delete"
-            onClick={() => navigate(`/${id}/confirm`)}
-          >
-            X
-          </button>
+            {/* delete receipt */}
+            <button
+              className="btn btn-delete"
+              onClick={() => navigate(`/${id}/confirm`)}
+            >
+              X
+            </button>
+          </div>
+          <p>PO Box 8412 - Market Mall, Calgary, AB, T3A 5C4</p>
+
+          <p>Charitable Reg. #85502 1739 RR 0001</p>
+          <p>
+            Canadian Revenue Agency: <a>www.cra-arc.gc.ca</a>
+          </p>
         </div>
-        <p>PO Box 8412 - Market Mall, Calgary, AB, T3A 5C4</p>
-
-        <p>Charitable Reg. #85502 1739 RR 0001</p>
-        <p>
-          Canadian Revenue Agency: <a>www.cra-arc.gc.ca</a>
-        </p>
-      </div>
-      <div className="donor">
-        <div>
-          <h3>
-            Date: {receiptDate}
-            {/* {new Date(receipt[0].createdAt).toLocaleString("en-GB", {
+        <div className="donor">
+          <div>
+            <h3>
+              Date: {receiptDate}
+              {/* {new Date(receipt[0].createdAt).toLocaleString("en-GB", {
               year: "numeric",
               month: "numeric",
               day: "numeric",
             })} */}
-          </h3>
-          <h3>Donation Location: {receipt[0].place}</h3>
-          <h3>
-            Donor: {receipt[0].firstName} {receipt[0].lastName}
-          </h3>
-          <h3>Address: {receipt[0].address},</h3>
-          <h3>Postal Code: {receipt[0].postalCode}</h3>
+            </h3>
+            <h3>Donation Location: {receipt[0].place}</h3>
+            <h3>
+              Donor: {receipt[0].firstName} {receipt[0].lastName}
+            </h3>
+            <h3>Address: {receipt[0].address},</h3>
+            <h3>Postal Code: {receipt[0].postalCode}</h3>
+          </div>
+          <div>
+            <h3>Donation Type: {receipt[0].type}</h3>
+            <h3>Amount: ${receipt[0].number}</h3>
+            <h3>Total Amount Received: {receipt[0].words} </h3>
+            <h3>Digital Signature: {receipt[0].signature}</h3>
+          </div>
+          <p>OFFICIAL RECEIPT FOR INCOME TAX PURPOSES</p>
         </div>
-        <div>
-          <h3>Donation Type: {receipt[0].type}</h3>
-          <h3>Amount: ${receipt[0].number}</h3>
-          <h3>Total Amount Received: {receipt[0].words} </h3>
-          <h3>Digital Signature: {receipt[0].signature}</h3>
-        </div>
-        <p>OFFICIAL RECEIPT FOR INCOME TAX PURPOSES</p>
       </div>
-
       {/* edit and Download receipt */}
       <div className="edit">
         <Link to={`/edit/${receipt[0]._id}`} className="btn btn-edit btn-block">
@@ -95,7 +112,7 @@ const ReceiptPage = () => {
           Download PDF
         </a>
       </div>
-    </div>
+    </main>
   );
 };
 export default ReceiptPage;
