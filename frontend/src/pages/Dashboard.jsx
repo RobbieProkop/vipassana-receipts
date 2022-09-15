272b16cd5173a0fecb7e06118d69d8f7c5df5e81
Robbie Prokop <robbieprokop@gmail.com>
2022-09-15 12:58:25 +0400

    begin adding search function

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import ReceiptItem from "../components/ReceiptItem";
@@ -13,6 +13,10 @@ const Dashboard = () => {
   const { receiptsArr, isLoading, isError, message } = useSelector(
     (state) => state.receipts
   );
+
+  //useStates
+  const [searchId, setSearchId] = useState("");
+  const [userId, setUserId] = useState("");
 
   useEffect(() => {
     if (!user) {
@@ -34,11 +38,40 @@ const Dashboard = () => {
       <section className="heading">
         <h1>Welcome {user && user.username}</h1>
         <p>Receipts Dashboard</p>
+        <Link to="/form">
+          {" "}
+          <button className="btn">Add A Receipt</button>
+        </Link>
+
+        {/* THIS IS NOT DOING A GET REQUEST BY POST ID, IT IS FILTERING THE POSTS INSTEAD. MAY CHANGE LATER */}
+        <div className="search">
+          <div className="column">
+            <label htmlFor="postSearch">Search by User</label>
+            <input
+              type="text"
+              name="user"
+              id="search-user"
+              value={userId}
+              placeholder="User:"
+              onChange={(e) => setUserId(e.target.value)}
+              disabled={searchId}
+            />
+          </div>
+
+          <div className="column">
+            <label htmlFor="postSearch">Search by ID</label>
+            <input
+              type="text"
+              name="searchId"
+              id="search-postId"
+              value={searchId}
+              placeholder="ID:"
+              onChange={(e) => setSearchId(e.target.value)}
+              disabled={userId}
+            />
+          </div>
+        </div>
       </section>
-      <Link to="/form">
-        {" "}
-        <button className="btn">Add A Receipt</button>
-      </Link>
       <section className="content">
         {receiptsArr.length > 0 ? (
           <div className="receipts">
