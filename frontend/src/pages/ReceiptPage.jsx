ea7edc62eaabfecd57b358a201b7b549871344f2
Robbie Prokop <robbieprokop@gmail.com>
2022-09-17 12:46:44 +0400

    trying to refactor receiptPage

--- frontend/src/pages/ReceiptPage.jsx
+++ frontend/src/pages/ReceiptPage.jsx
@@ -2,14 +2,19 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import jsPDF from "jspdf";
+import { getAll, getOneReceipt } from "../features/receipts/receiptSlice";
+import Spinner from "../components/Spinner";
 
 const ReceiptPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const receipt = useSelector((state) => {
-    return state.receipts.receiptsArr.filter((receipt) => receipt._id === id);
+  const { receiptsArr, isLoading, isError, message } = useSelector(
+    (state) => state.receipts
+  );
+  const receipt = receiptsArr.filter((receipt) => {
+    return receipt._id === id;
   });
 
   const receiptDate = new Date(receipt[0].createdAt).toLocaleString("en-GB", {
@@ -86,84 +91,97 @@ const ReceiptPage = () => {
 
     doc.setFontSize(16);
     doc.text(180, 350, `OFFICIAL RECEIPT FOR INCOME TAX PURPOSES`);
-
-    // doc.fromHTML(document.getElementById("receipt"), {
-    //   callback: function (doc) {
-    //     doc.save(`AVF-tax-receipt-${receiptDate}.pdf`);
-    //   },
-    // });
-
     doc.save(`AVF-tax-receipt-${receiptDate}.pdf`);
   };
 
-  useEffect(() => {}, [dispatch, receipt]);
+  useEffect(() => {
+    if (!receiptsArr.length) {
+      console.log("empty array");
+      dispatch(getAll());
+    }
+  }, [dispatch, receipt]);
+
+  console.log("empty?", receiptsArr);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
-    <main className="receipt-page">
-      <div id="receipt">
-        <div className="receipt-info">
-          <div className="receipt-head">
-            <h3>
-              N<sub>0</sub>. {receipt[0].receiptNumber}
-            </h3>
-            <h1>Alberta Vipassana Foundation</h1>
+    <>
+      {receiptsArr.length > 0 ? (
+        <main className="receipt-page">
+          <div id="receipt">
+            <div className="receipt-info">
+              <div className="receipt-head">
+                <h3>
+                  N<sub>0</sub>. {receipt[0].receiptNumber}
+                </h3>
+                <h1>Alberta Vipassana Foundation</h1>
 
-            {/* delete receipt */}
-            <button
-              className="btn btn-delete"
-              onClick={() => navigate(`/${id}/confirm`)}
-            >
-              X
-            </button>
-          </div>
-          <p>PO Box 8412 - Market Mall, Calgary, AB, T3A 5C4</p>
+                {/* delete receipt */}
+                <button
+                  className="btn btn-delete"
+                  onClick={() => navigate(`/${id}/confirm`)}
+                >
+                  X
+                </button>
+              </div>
+              <p>PO Box 8412 - Market Mall, Calgary, AB, T3A 5C4</p>
 
-          <p>Charitable Reg. #85502 1739 RR 0001</p>
-          <p>
-            Canadian Revenue Agency: <a>www.cra-arc.gc.ca</a>
-          </p>
-        </div>
-        <div className="donor">
-          <div>
-            <h3>
-              Date: {receiptDate}
-              {/* {new Date(receipt[0].createdAt).toLocaleString("en-GB", {
+              <p>Charitable Reg. #85502 1739 RR 0001</p>
+              <p>
+                Canadian Revenue Agency: <a>www.cra-arc.gc.ca</a>
+              </p>
+            </div>
+            <div className="donor">
+              <div>
+                <h3>
+                  Date: {receiptDate}
+                  {/* {new Date(receipt[0].createdAt).toLocaleString("en-GB", {
               year: "numeric",
               month: "numeric",
               day: "numeric",
             })} */}
-            </h3>
-            <h3>Location: {receipt[0].place}</h3>
-            <h3>
-              Donor: {receipt[0].firstName} {receipt[0].lastName}
-            </h3>
-            <h3>Address: {receipt[0].address},</h3>
-            <h3>Postal Code: {receipt[0].postalCode}</h3>
+                </h3>
+                <h3>Location: {receipt[0].place}</h3>
+                <h3>
+                  Donor: {receipt[0].firstName} {receipt[0].lastName}
+                </h3>
+                <h3>Address: {receipt[0].address},</h3>
+                <h3>Postal Code: {receipt[0].postalCode}</h3>
+              </div>
+              <div>
+                <h3>Donation Type: {receipt[0].type}</h3>
+                <h3>Amount: ${receipt[0].number}</h3>
+                <h3>Total Amount Received: {receipt[0].words} </h3>
+                <h3>Digital Signature: {receipt[0].signature}</h3>
+              </div>
+              <p>OFFICIAL RECEIPT FOR INCOME TAX PURPOSES</p>
+            </div>
           </div>
-          <div>
-            <h3>Donation Type: {receipt[0].type}</h3>
-            <h3>Amount: ${receipt[0].number}</h3>
-            <h3>Total Amount Received: {receipt[0].words} </h3>
-            <h3>Digital Signature: {receipt[0].signature}</h3>
-          </div>
-          <p>OFFICIAL RECEIPT FOR INCOME TAX PURPOSES</p>
-        </div>
-      </div>
-      {/* edit and Download receipt */}
-      <div className="edit">
-        <Link to={`/edit/${receipt[0]._id}`} className="btn btn-edit btn-block">
-          Edit
-        </Link>
+          // edit and Download receipt
+          <div className="edit">
+            <Link
+              to={`/edit/${receipt[0]._id}`}
+              className="btn btn-edit btn-block"
+            >
+              Edit
+            </Link>
 
-        <a
-          className="btn btn-down btn-block"
-          target="_blank"
-          onClick={generatePDF}
-        >
-          Download PDF
-        </a>
-      </div>
-    </main>
+            <a
+              className="btn btn-down btn-block"
+              target="_blank"
+              onClick={generatePDF}
+            >
+              Download PDF
+            </a>
+          </div>
+        </main>
+      ) : (
+        <h3>No Receipts to show</h3>
+      )}
+    </>
   );
 };
 export default ReceiptPage;
