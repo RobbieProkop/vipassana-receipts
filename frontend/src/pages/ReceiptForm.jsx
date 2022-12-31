ac8f274c46a232adc4a1d9f965ec2f3c85818830
Robbie Prokop <robbieprokop@gmail.com>
2022-12-31 10:11:55 -0700

    new number for receipt id

--- frontend/src/pages/ReceiptForm.jsx
+++ frontend/src/pages/ReceiptForm.jsx
@@ -17,7 +17,7 @@ const ReceiptForm = () => {
 
   // used to increament the receipts number
   const [receiptNumber, setReceiptNumber] = useState(
-    receipt.length ? receipt[0].receiptNumber + 1 : 6043
+    receipt.length ? receipt[0].receiptNumber + 1 : 5985
   );
 
   const [receiptData, setReceiptData] = useState({
