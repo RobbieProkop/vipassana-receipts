217c67d8c0dbab9b8b9a7c4ee667e73b7199b02b
Robbie Prokop <robbieprokop@gmail.com>
2023-01-07 22:32:02 -0700

    receipt form allow void and $0

--- frontend/src/pages/ReceiptForm.jsx
+++ frontend/src/pages/ReceiptForm.jsx
@@ -262,6 +262,7 @@ const ReceiptForm = () => {
                 <option value="Debit">Debit</option>
                 <option value="Cash">Cash</option>
                 <option value="Cheque">Cheque</option>
+                <option value="Void">Void</option>
               </select>
             </div>
             <div className="form-group">
@@ -270,7 +271,7 @@ const ReceiptForm = () => {
                 name="number"
                 id="number"
                 placeholder="Amount (Number)"
-                value={number <= 0 ? "" : number}
+                value={number < 0 ? "" : number}
                 onChange={onChange}
               />
             </div>
