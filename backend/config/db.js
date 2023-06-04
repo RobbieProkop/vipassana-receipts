8e660495bf59a82577c4bee81706c32e88bdcc93
firewallfail <firewall.fail@hotmail.com>
2023-06-04 18:43:15 -0400

    remove unused code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,5 @@
 require('dotenv').config();
 
-const { DB_HOST, DB_USER, DB_PORT, DB_PASSWORD, DB_NAME } = process.env;
-
-const { Sequelize, DataTypes, Op, QueryTypes } = require('sequelize');
-
-// Create connection to postgres
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres',
-  pool: {
-    max: 1,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
-
 // sequelize.query(
 //   `CREATE TABLE IF NOT EXISTS Receipts
 //   (
