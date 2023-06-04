11aa1cb2dd042c403b38f6e1110e252486e543f2
firewallfail <firewall.fail@hotmail.com>
2023-06-04 18:38:57 -0400

    table query

--- backend/config/db.js
+++ backend/config/db.js
@@ -16,6 +16,30 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   }
 });
 
+// sequelize.query(
+//   `CREATE TABLE IF NOT EXISTS Receipts
+//   (
+//     receipt_number SERIAL PRIMARY KEY,
+//     place VARCHAR(255),
+//     first_name VARCHAR(255),
+//     email VARCHAR(255),
+//     address VARCHAR(255),
+//     city VARCHAR(255),
+//     province VARCHAR(255),
+//     postal_code VARCHAR(255),
+//     type VARCHAR(255),
+//     number INT,
+//     words VARCHAR(255),
+//     signature VARCHAR(255),
+//     created_at timestamp with time zone DEFAULT now()
+//   );
+//   ALTER SEQUENCE receiptNumber
+//   RESTART WITH 100000;
+//   `, {
+//     raw: true
+//   }
+// )
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
